c1a9e87b5d42dcd2c1462125b114ee679dafb8d9
Nils Blömeke <bloemeni@outlook.com>
2019-12-06 19:15:19 +0100

    Working AR rotation -> IG motion

---
+++ WWFGameJam/Assets/Scripts/ARRotationManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+public class ARRotationManager : MonoBehaviour
+{
+
+    public GameObject TestObject;
+    public Transform ARObject;
+
+    private Quaternion previousRotation;
+    private Quaternion trota;
+
+    void Update()
+    {
+        TestObject.transform.SetPositionAndRotation(TestObject.transform.position, new Quaternion(trota.x, trota.y, ARObject.transform.rotation.y, trota.w));
+        previousRotation = ARObject.rotation;
+    }
+
+    private void Start()
+    {
+        trota = TestObject.transform.rotation;
+        previousRotation = ARObject.rotation;
+    }
+}
