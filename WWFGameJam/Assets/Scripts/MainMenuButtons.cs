26631a612d96e67c472ced73df44e050abe8f66c
Moritz Vogt <zocken223344@gmx.de>
2019-12-06 19:42:42 +0100

    Auto stash before merge of "master" and "origin/master"

--- WWFGameJam/Assets/Scripts/MainMenuButtons.cs
+++ WWFGameJam/Assets/Scripts/MainMenuButtons.cs
@@ -1,11 +1,24 @@
+<<<<<<< Updated upstream
 ﻿
+=======
+﻿using System.Collections;
+using System.Collections.Generic;
+>>>>>>> Stashed changes
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenuButtons : MonoBehaviour
 {
+<<<<<<< Updated upstream
     public void PlayButton()
     {
         SceneManager.LoadScene("GameScene");
+=======
+    [SerializeField] private Scene gameField;
+
+    public void PlayButton()
+    {
+        SceneManager.LoadScene("Scenes/SampleScene");
+>>>>>>> Stashed changes
     }
 }
