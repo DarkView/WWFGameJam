b417965c907961acdacbeaf9c9833f3a2ef3d3d5
Nils Blömeke <bloemeni@outlook.com>
2019-12-06 19:24:43 +0100

    Resolve merge conflict

--- WWFGameJam/Assets/Scripts/MainMenuButtons.cs
+++ WWFGameJam/Assets/Scripts/MainMenuButtons.cs
@@ -1,20 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenuButtons : MonoBehaviour
 {
-<<<<<<< Updated upstream
     public void PlayButton()
     {
         SceneManager.LoadScene("GameScene");
-=======
-    [SerializeField] private Scene gameField;
-
-    public void PlayButton()
-    {
-        SceneManager.LoadScene("Scenes/SampleScene");
->>>>>>> Stashed changes
     }
 }
