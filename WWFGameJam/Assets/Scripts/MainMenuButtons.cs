fd287ee8838ee9c4a3f5bd68d9936cff549184f4
ExeCûte <exe.cute@icloud.com>
2019-12-06 19:17:08 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ WWFGameJam/Assets/Scripts/MainMenuButtons.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButtons : MonoBehaviour
+{
+    [SerializeField] private Scene gameField;
+
+    public void PlayButton()
+    {
+        SceneManager.LoadScene("Scenes/SampleScene");
+    }
+}
