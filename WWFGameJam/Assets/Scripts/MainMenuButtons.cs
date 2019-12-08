5b96361ecfd7f256ecc2c234e87a0b5cf89a511b
ExeCûte <exe.cute@icloud.com>
2019-12-08 12:12:06 +0100

    Ui Finish

--- WWFGameJam/Assets/Scripts/MainMenuButtons.cs
+++ WWFGameJam/Assets/Scripts/MainMenuButtons.cs
@@ -3,14 +3,30 @@ using System.Net.Mime;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using  UnityEngine.UI;
+using Random = System.Random;
 
 public class MainMenuButtons : MonoBehaviour
 {
     private int gamesPlayed;
+    [SerializeField] private GameObject spucke;
+    [SerializeField] private GameObject ohneSpucke;
+    private int background;
 
     private void Start()
     {
         gamesPlayed = PlayerPrefs.GetInt("GamesPlayedKey", 0);
+        background = UnityEngine.Random.Range(1, 100);
+        if (background < 90)
+        {
+            spucke.SetActive(true);
+            ohneSpucke.SetActive(false);
+        }
+        else
+        {
+            spucke.SetActive(false);
+            ohneSpucke.SetActive(true);
+        }
+
     }
 
     public void PlayButton()
